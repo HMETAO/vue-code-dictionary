e111c0ebe8dcd8aee877b1de6183512633397aed
HMETAO <1430271732@qq.com>
2022-07-10 20:33:29 +0800

    feat(login): 登录页样式

---
+++ src/view/Login.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class='login-container'>
+    <div class='login-Box'>
+      <!-- 头像 -->
+      <div class='avatar-Box'>
+        <img src='../assets/img/hmetao.png' alt='hmetao' />
+      </div>
+      <!-- 表单 -->
+      <el-form
+        label-width='0px'
+        class='login-form'
+        :model='loginForm'
+        ref='loginFormRef'
+      >
+        <!-- 用户名 -->
+        <el-form-item prop='username'>
+          <el-input
+            placeholder='请输入用户名'
+            prefix-icon='el-icon-user'
+            v-model='loginForm.username'
+          ></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop='password'>
+          <el-input
+            placeholder='请输入密码'
+            prefix-icon='el-icon-lock'
+            v-model='loginForm.password'
+            type='password'
+          ></el-input>
+        </el-form-item>
+        <!-- 按钮区域 -->
+        <el-form-item class='login-btn'>
+          <el-button type='primary' @click='login()'>登录</el-button>
+          <el-button type='info' @click='resetLoginForm()'>重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      loginForm: {}
+    }
+  }
+}
+</script>
+
+<style lang='less' scoped>
+
+.login-btn {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.login-form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 20px;
+  box-sizing: border-box;
+}
+
+.login-container {
+  background-color: #74b9ff;
+  height: 100%;
+
+  .login-Box {
+    position: relative;
+    width: 450px;
+    height: 300px;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #f1f2f6;
+    border-radius: 8px;
+
+    .avatar-Box {
+      position: absolute;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 80px;
+      height: 80px;
+      border: 1px solid #eeeeee;
+      border-radius: 50%;
+      padding: 10px;
+      overflow: hidden;
+      box-shadow: 0 0 10px #dddddd;
+      background-color: #f1f2f6;
+
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        background: #dfe6e9;
+      }
+    }
+  }
+}
+
+</style>
